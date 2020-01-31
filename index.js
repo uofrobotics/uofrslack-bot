08af877a8b630adc7e6a2a9379cd8dc69ed8c1c0
lite20 <lite260@gmail.com>
2020-01-31 16:45:06 -0600

    express

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const { RTMClient } = require('@slack/rtm-api');
 const dotenv = require('dotenv')
+const express = require('express')
+
+const app = express()
+const port = 3000
 
 dotenv.config()
 
-// Initialize
-const rtm = new RTMClient(`${process.env.BOT_TOKEN}`);
+app.get('/', (req, res) => res.send('Hello World!'))
 
-rtm.on('message', (event) => {
-    console.log(event);
-});
-
-(async () => {
-    await rtm.start();
-})();
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
